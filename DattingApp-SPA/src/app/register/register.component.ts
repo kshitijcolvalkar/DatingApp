d4805de0e7d541db130c0866cc46c8a541b77b13
Kshitij Colvalkar <kshitij.colvalkar@3dsystems.com>
2020-05-19 22:32:42 +0530

    End of section Third party components

--- DattingApp-SPA/src/app/register/register.component.ts
+++ DattingApp-SPA/src/app/register/register.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 import { AuthService } from '../_services/auth.service';
+import { AlertifyService } from '../_services/alertify.service';
 
 @Component({
   selector: 'app-register',
@@ -10,7 +11,7 @@ export class RegisterComponent implements OnInit {
 
   @Output() cancelRegister = new EventEmitter();
   model: any = {};
-  constructor(private authService: AuthService) { }
+  constructor(private authService: AuthService, private alertify: AlertifyService) { }
 
   ngOnInit() {
   }
@@ -18,16 +19,16 @@ export class RegisterComponent implements OnInit {
   register()
   {
     this.authService.register(this.model).subscribe(() => {
-      console.log('Registered Successfully');
+      this.alertify.success('Registered Successfully');
     }, error => {
-      console.log(error);
+      this.alertify.error(error);
     });
   }
 
   cancel()
   {
     this.cancelRegister.emit(false);
-    console.log('cancelled');
+    this.alertify.warning('cancelled');
   }
 
 }
